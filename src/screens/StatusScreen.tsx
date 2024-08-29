0d35a8aaf06a0cd46d502f8483f7e6753b5c7c3c
Swarup Das <swarupd049@gmail.com>
2024-08-29 19:27:26 +0530

    added icons pack

--- src/screens/StatusScreen.tsx
+++ src/screens/StatusScreen.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
 
 interface StatusScreenProps {}
 
@@ -7,6 +9,7 @@ const StatusScreen = (props: StatusScreenProps) => {
   return (
     <View style={styles.container}>
       <Text>StatusScreen</Text>
+      <MaterialIcons name="home" size={30} color="#000" />
     </View>
   );
 };
